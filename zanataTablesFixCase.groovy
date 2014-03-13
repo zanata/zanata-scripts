dc30ef1bd19fb278fdc8601205a0c10cecb2bffa
Sean Flanigan <sflaniga@redhat.com>
2014-03-14 09:40:47 +1000

    Remove changelog header

--- zanataTablesFixCase.groovy
+++ zanataTablesFixCase.groovy
@@ -5,21 +5,6 @@
  * Unix filter script to convert lower case Zanata table names back to their original (title) case in an SQL backup
  *
  * Usage: zcat zanata_backup.sql.gz | zanata-tables-fix-case.groovy | gzip -c > zanata_backup.casefixed.sql.gz
- *
- * Version 3
- * Date: 7 Nov 2013
- * Author: Sean Flanigan <sflaniga@redhat.com>
- *
- * Changelog:
- *
- * 7 Nov 2013 Sean Flanigan <sflaniga@redhat.com>
- * Bug fix: need to replace long table names first
- *
- * 5 Nov 2013 Sean Flanigan <sflaniga@redhat.com>
- * Check for unknown table names
- *
- * 4 Nov 2013 Sean Flanigan <sflaniga@redhat.com>
- * Initial version
  */
 
 // Table list generated with this commend:
