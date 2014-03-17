30dd3f0b2ee812047bf3a9a4099d50475abc7b2d
Patrick Huang <pahuang@redhat.com>
2014-03-17 15:14:41 +1000

    add load test script

---
+++ getTranslationLoadTest.groovy
@@ -0,0 +1,67 @@
+#!/usr/bin/env groovy
+package org.zanata.adhoc
+
+@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7')
+
+import groovyx.net.http.RESTClient
+
+def cli = new CliBuilder(usage:'get translation from zanata (sort of load testing)')
+cli.help('print help')
+cli.h(longOpt: 'host', 'zanata host (default http://localhost:8080/zanata)')
+cli.p(longOpt: 'project', 'targeting project (default skynet-topics)')
+cli.i(longOpt: 'iteration', 'targeting project iteration (default 1)')
+cli.u(longOpt: 'username', 'username (default admin)')
+cli.k(longOpt: 'apiKey', 'API key (default b6d7044e9ee3b2447c28fb7c50d86d98)')
+
+def options = cli.parse(args)
+
+if (options.help) {
+    cli.usage()
+    System.exit(1)
+}
+
+def zanataInstance = options.h ? options.h : "http://localhost:8080/zanata"
+def zanataRestUrl = "$zanataInstance/rest/"
+
+def zanataRestClient = new RESTClient(zanataRestUrl, "application/xml")
+zanataRestClient.handler.failure = {
+    it
+}
+
+def projectSlug = options.p ? options.p : 'skynet-topics'
+def versionSlug = options.i ? options.i : '1'
+def authHeaders = ['X-Auth-User': "admin", 'X-Auth-Token': "b6d7044e9ee3b2447c28fb7c50d86d98"]
+
+// get all resource names
+def resourcePath = "projects/p/$projectSlug/iterations/i/$versionSlug/r"
+def response = zanataRestClient.get(headers: authHeaders, path : resourcePath)
+assert response.status == 200 : "Error getting resource for $projectSlug/$versionSlug at $resourcePath"
+
+def namespaces = ['http://zanata.org/namespace/api/': 'api']
+def resources = response.data.declareNamespace(namespaces)
+def names = resources.'resource-meta'.collect {
+    it.name
+}
+
+println "resource names: $names"
+
+// get all available locales
+def statsResponse = zanataRestClient.get(headers: authHeaders, path: "stats/proj/$projectSlug/iter/$versionSlug")
+assert statsResponse.status == 200 : "Error getting statistics for $projectSlug/$versionSlug"
+
+def locales = statsResponse.data.declareNamespace(namespaces).stats.stat.'@locale'.collect {
+    it
+}
+
+println "locales: $locales"
+
+// get translations
+println "getting translations for all these locales and resources"
+locales.each { locale ->
+    names.each { resId ->
+        def msg = "getting translation for $resId for locale $locale"
+        def resp = zanataRestClient.get(headers: authHeaders, path: "$resourcePath/$resId/translations/$locale")
+        // we don't really care the response content
+        println "$msg -> $resp.status"
+    }
+}
